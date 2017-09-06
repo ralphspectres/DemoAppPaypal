5981f0b5240803733d5b4a5792c3c7ceec9f879f
ralphlaurenagustin <ralphlauren.agustin@benilde.edu.ph>
2017-09-06 14:22:54 +0800

    experimented on controllers and models

---
+++ DemoApp/Controllers/PaypalController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class PaypalController : Controller
+    {
+        public ActionResult RedirectFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult CancelFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult NotifyFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult ValidateCommand(string product, string totalPrice)
+        {
+            return View();
+        }
+	}
+}
--- DemoApp/Models/PaypalModel.cs
+++ DemoApp/Models/PaypalModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,6 @@ namespace DemoApp.Models
 {
     public class PaypalModel
     {
-        public PaypalModel()
-        {
-
-        }
         public string cmd { get; set; }
         public string business { get; set; }
         public string no_shipping { get; set; }
@@ -20,6 +17,26 @@ namespace DemoApp.Models
         public string currency_code { get; set; }
         public string item_name { get; set; }
         public string amount { get; set; }
+        public string actionURL { get; set; }
 
+        public PaypalModel(bool useSandbox)
+        {
+            this.cmd = "_xclick";
+            this.business = ConfigurationManager.AppSettings["business"];
+            this.cancel_return = ConfigurationManager.AppSettings["cancel_return"];
+            this.@return = ConfigurationManager.AppSettings["return"];
+            if (useSandbox)
+            {
+            this.actionURL = ConfigurationManager.AppSettings["test_url"];
+            }
+            else
+            {
+            this.actionURL = ConfigurationManager.AppSettings["Prod_url"];
+            }
+            
+            this.notify_url = ConfigurationManager.AppSettings["notify_url"];
+            
+            this.currency_code = ConfigurationManager.AppSettings["currency_code"];
+        }
     }
 }
